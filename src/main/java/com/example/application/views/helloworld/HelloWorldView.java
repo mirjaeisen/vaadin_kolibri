9a0c6b2a3b2b4a4d5bda03549f8b14e604b116f3
m.eisenblaetter <m.eisenblaetter@conterra.de>
2023-12-12 07:23:43 +0100

    Example for problem with setValue of KolSelect

--- src/main/java/com/example/application/views/helloworld/HelloWorldView.java
+++ src/main/java/com/example/application/views/helloworld/HelloWorldView.java
@@ -1,9 +1,6 @@
 package com.example.application.views.helloworld;
 
-import com.example.adapters.KolButton;
-import com.example.adapters.KolInputText;
 import com.example.adapters.KolSelect;
-import com.example.adapters.KolTabs;
 import com.example.application.views.MainLayout;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -14,8 +11,6 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.router.RouteAlias;
 
-import java.text.DecimalFormat;
-
 @PageTitle("Hello World")
 @Route(value = "hello", layout = MainLayout.class)
 @RouteAlias(value = "", layout = MainLayout.class)
@@ -25,61 +20,70 @@ public class HelloWorldView extends VerticalLayout {
      * This elements are needed to sync the form field value from
      * to web component shadow root, to a vaadin accessible context.
      */
-    Element inputText = ElementFactory.createInput("text");
     Element select = ElementFactory.createSelect();
 
-    private void handleInputTextChange(DomEvent event) {
-        Notification.show(event.getEventData().getString("event.detail"));
-    }
+    private final KolSelect refSystemIn;
+    private final KolSelect koordSystemIn;
+    private final String blhLabel = "geographische Koordinaten";
+    private final String gkLabel = "Gauß-Krüger Koordinaten";
+
+    private final String koordSystemListGKBLH = "[{'label':'" + gkLabel + "','value':'gaußkruegerkoordinaten'}," +
+            "{'label':'" + blhLabel + "','value':'geographischekoordinaten'}]";
 
     private void handleSelectChange(DomEvent event) {
         Notification.show(event.getEventData().get("event.detail").asString());
     }
 
-    private void handleClick(DomEvent event) {
-        Notification.show("Clicked!");
-    }
-
     public HelloWorldView() {
-        setMargin(true);
+        setSpacing(false);
 
-        KolTabs kolTabs = new KolTabs();
-        kolTabs.setSelected("1");
-        kolTabs.setTabsAlign("bottom");
-        kolTabs.setTabs(
-                "[{\"_label\":\"Koordinatenpaar/Koordinatenliste\",\"_href\":\"startseite\",\"_active\":true, \"_icon\":\"\"},{\"_label\":\"ESRI SHAPE-DATEIEN\",\"_href\":\"2-menuepunkt\", \"_icon\":\"\"},{\"_label\":\"CSV-Dateien\",\"_href\":\"startseite\", \"_icon\":\"\"}]");
-        add(kolTabs);
-        kolTabs.getElement().addEventListener("kol-change",  evt->{
-            System.out.println(evt.getEventData());
-            double number = evt.getEventData().getNumber("event.detail");
-            DecimalFormat df = new DecimalFormat("0");
-            Notification.show(df.format(number));
-        }).addEventData("event.detail");
+        // Eingabeparameter
+        var parameterIn = new VerticalLayout();
+        parameterIn.getElement().getStyle().set("padding", "0px");
+        parameterIn.getElement().getStyle().set("width", "100%");
 
-        KolInputText kolInputText = new KolInputText();
-        kolInputText.setLabel("Vorname");
-        kolInputText.getElement().addEventListener("kol-change",this::handleInputTextChange).addEventData("event.detail");
-        kolInputText.getElement().setAttribute("_sync-value-by-selector", "#kol-input-text");
-        add(kolInputText);
+        refSystemIn = new KolSelect();
+        String refSystemList = "[{'label':'" + "DHDN" + "','value':'dhdn'}," +
+                "{'label':'" + "STN" + "','value':'stn'}," +
+                "{'label':'" + "ETRS" + "','value':'etrs'}]";
+        refSystemIn.setOptions(refSystemList);
+        refSystemIn.setHint("Referenzsystem");
+        refSystemIn.getElement().getStyle().set("width", "250px");
+        refSystemIn.setValue("['stn']");
+        refSystemIn.getElement().addEventListener("kol-change", this::handleRefSystemInChanged).addEventData("event.detail");
+        refSystemIn.getElement().setAttribute("_sync-value-by-selector", "#kol-select");
+        add(refSystemIn);
 
-
-        KolSelect koordSystemIn = new KolSelect();
-        koordSystemIn.setList(
-                "[{'label':'Gauß-Krüger Koordinaten','value':'GKK'},{'label':'Geographische Koordinaten','value':'GK'}]");
+        koordSystemIn = new KolSelect();
+        var koordSystemList = "[{'label':'" + "UTM6" + "','value':'utm6'}, " +
+                "{'label':'" + "UTM7" + "','value':'utm7'}," +
+                "{'label':'" + "UTM8" + "','value':'utm8'}," +
+                "{'label':'" + blhLabel + "','value':'geographischekoordinaten'}," +
+                "{'label':'" + "Kartesische Koordinaten" + "','value':'xyz'}," +
+                "{'label':'" + gkLabel + "','value':'gaußkruegerkoordinaten'}]";
+        koordSystemIn.setOptions(koordSystemList);
+        koordSystemIn.setValue("['gaußkruegerkoordinaten']");
         koordSystemIn.setHint("Koordinatensystem");
         koordSystemIn.getElement().addEventListener("kol-change", this::handleSelectChange).addEventData("event.detail");
         koordSystemIn.getElement().setAttribute("_sync-value-by-selector", "#kol-select");
+        koordSystemIn.getElement().getStyle().set("width", "250px");
         add(koordSystemIn);
 
-        KolButton kolButton = new KolButton();
-        kolButton.setLabel("Klick mich!");
-        kolButton.setValue("Schalter 1");
-        /**
-         * Bug in rc.23
-         * kolButton.getElement().addEventListener("kol-click", this::handleClick);
-         */
-        kolButton.getElement().addEventListener("kol-change", this::handleClick);
-        add(kolButton);
+        add(parameterIn);
     }
 
+    public void handleRefSystemInChanged(DomEvent event) {
+        var selectedValue = (event.getEventData().get("event.detail").asString());
+        if (selectedValue.equals("dhdn")) {
+            koordSystemIn.setOptions(koordSystemListGKBLH);
+            koordSystemIn.setValue("['gaußkruegerkoordinaten']");
+        } else if (selectedValue.equals("stn")) {
+            koordSystemIn.setOptions(koordSystemListGKBLH);
+            System.out.println(koordSystemIn.getValue());
+            koordSystemIn.setValue("['gaußkruegerkoordinaten']"); // This sets the value but does no update the UI
+            System.out.println(koordSystemIn.getValue());
+        } else if (selectedValue.equals("etrs")) {
+            return;
+        }
+    }
 }
